fa86790ade8e3db1e06acc8cff0dbc6c4800da45
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-19 12:28:43 -0300

    update

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-const searchInput = document.getElementById("searchInput");
-const searchLabel = document.getElementById("searchLabel");
-const priceRange = document.getElementById("price");
-const priceValue = document.getElementById("priceValue");
 const menuIcon = document.getElementById("menuIcon");
 const navBar = document.getElementById("navigator-list");
 const ul = document.getElementById("list");
-
-priceValue.textContent = "$200,00 - $1000,00";
-
-priceRange.addEventListener("change", () => {
-    if (priceRange.value === "1000") {
-        priceValue.textContent = `$${priceRange.value},00`;
-    } else {
-        priceValue.textContent = `$${priceRange.value},00 - $1000,00`;
-    }
-});
-
-searchInput.addEventListener("focusin", () => {
-    searchLabel.style.backgroundColor = "#f9e6e6";
-    searchInput.placeholder = "";
-});
-
-searchInput.addEventListener("focusout", () => {
-    searchLabel.style.backgroundColor = "#f9e6e652";
-    searchInput.placeholder = "Search for";
-    if (searchInput.value !== "") {
-        searchInput.style.color = "rgba(0, 0, 0, 0.795)";
-    }
-});
 
 const listItems = ["Products", "Contact", "About Us"];
 navBar.style.display = "none";
@@ -39,8 +12,20 @@ menuIcon.addEventListener("click", () => {
         for (const item of listItems) {
             const li = document.createElement("li");
             const a = document.createElement("a");
-            a.href = "#";
-            a.textContent = item;
+            if (item === "Products") {
+                a.href = "#products";
+                a.textContent = item;
+            } else if (item === "Contact") {
+                a.href = "#products";
+                a.textContent = item;
+            } else if (item === "About Us") {
+                a.href = "#aboutus";
+                a.textContent = item;
+            } else {
+                const para = document.createElement("p");
+                para.textContent = "No Shit!";
+                navBar.appendChild(para);
+            }
             li.appendChild(a);
             ul.appendChild(li);
         }
