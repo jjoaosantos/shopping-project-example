559a9e8a6a1047f6693ceed7a7c71fce7c515f2f
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-20 11:29:02 -0300

    style part 10

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-const searchInput = document.getElementById("searchInput");
-const searchLabel = document.getElementById("searchLabel");
-const priceRange = document.getElementById("price");
-const priceValue = document.getElementById("priceValue");
 const menuIcon = document.getElementById("menuIcon");
 const navBar = document.getElementById("navigator-list");
 const ul = document.getElementById("list");
-
-priceValue.textContent = "$200,00 - $1000,00";
-
-priceRange.addEventListener("change", () => {
-    if (priceRange.value === "1000") {
-        priceValue.textContent = `$${priceRange.value},00`;
-    } else {
-        priceValue.textContent = `$${priceRange.value},00 - $1000,00`;
-    }
-});
-
-searchInput.addEventListener("focusin", () => {
-    searchLabel.style.backgroundColor = "#f9e6e6";
-    searchInput.placeholder = "";
-});
-
-searchInput.addEventListener("focusout", () => {
-    searchLabel.style.backgroundColor = "#f9e6e652";
-    searchInput.placeholder = "Search for";
-    if (searchInput.value !== "") {
-        searchInput.style.color = "rgba(0, 0, 0, 0.795)";
-    }
-});
 
 const listItems = ["Products", "Contact", "About Us"];
 navBar.style.display = "none";
@@ -43,7 +16,7 @@ menuIcon.addEventListener("click", () => {
                 a.href = "products.html";
                 a.textContent = item;
             } else if (item === "Contact") {
-                a.href = "contact.html";
+                a.href = "products.html";
                 a.textContent = item;
             } else if (item === "About Us") {
                 a.href = "aboutus.html";
