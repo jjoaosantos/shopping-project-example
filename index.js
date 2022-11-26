a91ef7714140981eecf0711a096dcc5bcb02aa90
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-26 20:37:49 -0300

    adjusting the callout code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const rememberDiv = document.querySelector(".remember");
+const forgetDiv = document.querySelector(".forget");
 const form = document.querySelector(".callout");
+const h2 = document.querySelector("#calloutHeading");
 const nameInput = document.querySelector("#accountName");
 const submitBtn = document.querySelector("#submitname");
 const forgetBtn = document.querySelector("#forgetname");
-
 
 const enterAccount = document.querySelector("#userInfo");
 
@@ -37,13 +39,15 @@ function nameDisplayCheck() {
     if (localStorage.getItem("name")) {
         const name = localStorage.getItem("name");
         enterAccount.textContent = `Hello, ${name}`;
-        submitBtn.style.display = "none";
-        forgetBtn.style.display = "block";
-        nameInput.style.display = "none";
+        h2.textContent = "Are you leaving?";
+        forgetDiv.style.display = "block";
+        rememberDiv.style.display = "none";
         form.style.display = "none";
     } else {
-        submitBtn.style.display = "block";
-        forgetBtn.style.display = "none";
+        enterAccount.textContent = "Sign In";
+        h2.textContent = "Want to join us?";
+        forgetDiv.style.display = "none";
+        rememberDiv.style.display = "block";
         form.style.display = "none";
     }
 }
