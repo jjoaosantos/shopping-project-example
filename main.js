a2e4ece820facc7c614f1fd59890b184299584eb
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-27 15:44:20 -0300

    update

--- main.js
+++ main.js
@@ -3,32 +3,12 @@ const navBar = document.getElementById("menuNavigator");
 
 navBar.style.display = "none";
 
-menuIcon.addEventListener("click", () => {
-    if (navBar.getAttribute("aria-hidden") === "true") {
-        navBar.setAttribute("aria-hidden", "false");
+menuBtn.addEventListener("click", () => {
+    if (menuBtn.getAttribute("aria-expanded") === "false") {
+        menuBtn.setAttribute("aria-expanded", "true");
         navBar.style.display = "block";
-        for (const item of listItems) {
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            if (item === "Products") {
-                a.href = "#products";
-                a.textContent = item;
-            } else if (item === "Contact") {
-                a.href = "#products";
-                a.textContent = item;
-            } else if (item === "About Us") {
-                a.href = "#aboutus";
-                a.textContent = item;
-            } else {
-                const para = document.createElement("p");
-                para.textContent = "No Shit!";
-                navBar.appendChild(para);
-            }
-            li.appendChild(a);
-            ul.appendChild(li);
-        }
-    } else if (navBar.getAttribute("aria-hidden") === "false") {
-        navBar.setAttribute("aria-hidden", "true");
+    } else if (menuBtn.getAttribute("aria-expanded") === "true") {
+        menuBtn.setAttribute("aria-expanded", "false");
         navBar.style.display = "none";
     }
 });
