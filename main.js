cac96fdb69ba18cdbeeab949a0f8c2f95e9d2af8
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-29 19:01:12 -0300

    styling the menu bar and search filter

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ navBar.style.display = "none";
 menuBtn.addEventListener("click", () => {
     if (menuBtn.getAttribute("aria-expanded") === "false") {
         menuBtn.setAttribute("aria-expanded", "true");
+        menuBtn.style.backgroundImage = "url('../images/x-icon.svg')";
+        menuBtn.style.backgroundSize = "80%";
         navBar.style.display = "block";
     } else if (menuBtn.getAttribute("aria-expanded") === "true") {
         menuBtn.setAttribute("aria-expanded", "false");
+        menuBtn.style.backgroundImage = "url('../images/menu-bar.svg')";
+        menuBtn.style.backgroundSize = "contain";
         navBar.style.display = "none";
     }
 });
