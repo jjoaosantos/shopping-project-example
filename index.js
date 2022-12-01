1af0309462fdea30a50fab7295b30734042a0d44
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-11-30 22:36:26 -0300

    adjusting the menu bar

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const rememberDiv = document.querySelector(".remember");
 const forgetDiv = document.querySelector(".forget");
 const form = document.querySelector(".callout");
-const h2 = document.querySelector("#calloutHeading");
-const nameInput = document.querySelector("#accountName");
+const personalGreeting = document.querySelector(".personal-greeting");
+const nameInput = document.querySelector("#entername");
 const submitBtn = document.querySelector("#submitname");
 const forgetBtn = document.querySelector("#forgetname");
 
-const enterAccount = document.querySelector("#userInfo");
-
 form.addEventListener("submit", (e) => e.preventDefault());
-
-document.querySelector("#closeBtn").addEventListener("click", () => {
-    form.setAttribute("aria-hidden", "true");
-    form.style.display = "none";
-    enterAccount.focus();
-});
 
 submitBtn.addEventListener("click", () => {
     localStorage.setItem("name", nameInput.value);
@@ -26,35 +18,17 @@ forgetBtn.addEventListener("click", () => {
     nameDisplayCheck();
 });
 
-enterAccount.addEventListener("click", () => {
-    if (form.getAttribute("aria-hidden") === "true") {
-        form.setAttribute("aria-hidden", "false");
-        form.style.display = "block";
-    } else if (profileIcon.getAttribute("aria-expanded") === "true") {
-        profileIcon.setAttribute("aria-expanded", "false")
-        form.style.display = "none";
-    }
-
-    forgetBtn.focus();
-    nameInput.focus();
-});
-
 function nameDisplayCheck() {
     if (localStorage.getItem("name")) {
         const name = localStorage.getItem("name");
-        enterAccount.textContent = `Hello, ${name}`;
-        h2.textContent = "Are you leaving?";
+        personalGreeting.textContent = `Hello, ${name}`;
         forgetDiv.style.display = "block";
         rememberDiv.style.display = "none";
-        form.style.display = "none";
     } else {
-        enterAccount.textContent = "Sign In";
-        h2.textContent = "Want to join us?";
+        personalGreeting.style.display = "none";
         forgetDiv.style.display = "none";
         rememberDiv.style.display = "block";
     }
-    enterAccount.focus();
-    nameInput.focus();
 }
 
 nameDisplayCheck();
