a3f0be160db7e4f72c2ba2a6cceb272b6d2d3d5d
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-12-03 21:33:20 -0300

    profile image icon gallery working

--- scripts/main.js
+++ scripts/main.js
@@ -26,15 +26,70 @@ menuBtn.addEventListener("click", () => {
 });
 
 
-img.style.opacity = "1";
-cart.style.opacity = "0";
+// img.style.opacity = "1";
+// cart.style.opacity = "0";
 
-container.addEventListener("click", () => {
-    if (img.style.opacity === "1") {
-        img.style.opacity = "0.3";
-        cart.style.opacity = "1";
-    } else if (img.style.opacity === "0.3") {
-        img.style.opacity = "1";
-        cart.style.opacity = "0";
-    }
-});
+// container.addEventListener("click", () => {
+//     if (img.style.opacity === "1") {
+//         img.style.opacity = "0.3";
+//         cart.style.opacity = "1";
+//     } else if (img.style.opacity === "0.3") {
+//         img.style.opacity = "1";
+//         cart.style.opacity = "0";
+//     }
+// });
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+const textArea = document.querySelector("#textarea");
+
+document.querySelector("#editingTool").addEventListener("click", () => {
+    textArea.disabled = "false";
+    textArea.ariaDisabled = "false";
+})
+
+const images = ["david-profile-icon.png", "josh-profile-icon.png", "john-profile-icon.png", "steve-profile-icon.png", "rebeca-profile-icon.png", "miranda-profile-icon.png", "jennifer-profile-icon.png", "julia-profile-icon.png"];
+
+const alts = {
+    "david-profile-icon.png" : "Bearded man with glasses smiling",
+    "josh-profile-icon.png" : "Green haired nerdy boy smiling",
+    "john-profile-icon.png" : "Orange hair man with glasses smiling",
+    "steve-profile-icon.png" : "Brown haired man wearing a lab coat smiling",
+    "rebeca-profile-icon.png" : "Nerdy girl with glasses smiling",
+    "miranda-profile-icon.png" : "Orange long hair woman smiling",
+    "jennifer-profile-icon.png" : "Girl with long dark hair smiling",
+    "julia-profile-icon.png" : "Girl with pink ponytail hair smiling"
+}
+
+const info = {
+    "david-profile-icon.png" : "Bearded man with glasses smiling.",
+    "josh-profile-icon.png" : "Green haired nerdy boy smiling.",
+    "john-profile-icon.png" : "Orange hair man with glasses smiling.",
+    "steve-profile-icon.png" : "Brown haired man wearing a lab coat smiling.",
+    "rebeca-profile-icon.png" : "Nerdy girl with glasses smiling.",
+    "miranda-profile-icon.png" : "Orange long hair woman smiling.",
+    "jennifer-profile-icon.png" : "Girl with long dark hair smiling.",
+    "julia-profile-icon.png" : "Girl with pink ponytail hair smiling."
+}
+
+for (const image of images) {
+    const newImage = document.createElement("img");
+    const section = document.createElement("section");
+    newImage.setAttribute("src", `../images/${image}`);
+    newImage.setAttribute("alt", alts[image]);
+    newImage.setAttribute("role", "button");
+    newImage.setAttribute("tabindex", "0");
+    thumbBar.appendChild(section);
+    section.appendChild(newImage);
+    
+    newImage.addEventListener("click", () => {
+        displayedImage.setAttribute("src", `../images/${image}`);
+        displayedImage.setAttribute("alt", `${alts[image]}`);
+        textArea.textContent = info[image];
+    })
+
+    newImage.addEventListener("keypress", () => {
+        displayedImage.setAttribute("src", `../images/${image}`);
+        displayedImage.setAttribute("alt", `${alts[image]}`);
+    })
+}
