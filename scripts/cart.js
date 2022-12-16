7c237e90b1dab9e051489b96c1a499409849e4fc
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-12-16 17:10:01 -0300

    cart page now can show the products

--- scripts/cart.js
+++ scripts/cart.js
@@ -11,37 +11,74 @@ fetch("../scripts/products.json")
           if (!localStorage.getItem("cart")) {
             localStorage.setItem("cart", "[]");
           }
+          initialize(data);
          } )
   
   .catch( err => console.error(`Fetch problem: ${err.message}`) );
 
-  const cart = JSON.parse(localStorage.getItem("cart"));
-  const cartIcon = document.querySelector(".cart-icon");
-  const main = document.querySelector("main");
+function initialize(products) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const cartIcon = document.querySelector(".cart-icon");
+    const main = document.querySelector("main");
 
-  let finalGroup = [];
-  finalGroup = cart.filter( product => product.image.includes(product.image));
-  console.log(finalGroup);
+    let finalGroup = [];
+    finalGroup = cart.filter( product => product.image.includes(product.image));
+    updateDisplay();
 
-  // function addItemToCart() {
-  //   // const item = cart.filter( item => {
-  //   //   return item.image;
-  //   // });
+    function updateDisplay() {
+      while (main.firstChild) {
+        main.removeChild(main.firstChild);
+      }
+  
+      if (finalGroup.length === 0) {
+        const para = document.createElement('p');
+        para.textContent = 'No results to display!';
+        main.appendChild(para);
+      } else {
+        for (const product of finalGroup) {
+          fetchBlob(product);
+        }
+      }
+    }
 
-  //   const product = cart.filter( item => item.image.includes(item.image));
+    function fetchBlob(product) {
+      const url = `../images/${product.image}`;
+      fetch(url)
+        .then( response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+          }
+          return response.blob();
+        })
+        .then( blob => showProduct(blob, product) )
+        .catch( err => console.error(`Fetch problem: ${err.message}`) );
+    }
 
-  //   const section = document.createElement("section");
-  //   const image = document.createElement("img");
-  //   // section.setAttribute("class", `${cart.branding}`)
-  //   // image.src = `../images/${product}`;
-  //   main.appendChild(section);
-  //   section.appendChild(image);
-  //   console.log(product);
-  // }
+    function showProduct(blob, product) {
+      const objectURL = URL.createObjectURL(blob);
+      const section = document.createElement('section');
+      const heading = document.createElement('h2');
+      const para = document.createElement('p');
+      const image = document.createElement('img');
+  
+      section.setAttribute('class', product.branding);
+  
+      image.src = objectURL;
+      image.alt = product.name;
+  
+      heading.textContent = product.name.replace(product.name.charAt(0), product.name.charAt(0).toUpperCase());
+  
+      para.textContent = `$${product.price}`;
+   
+      main.appendChild(section);
+      section.appendChild(image);
+      section.appendChild(heading);
+      section.appendChild(para);
+    }
 
-  // addItemToCart();
+    getTotal();
 
-  function getTotal() {
+    function getTotal() {
     const temp = cart.map( item => {
       return parseInt(item.quantity);
       // return parseInt(item.price);
@@ -53,4 +90,4 @@ fetch("../scripts/products.json")
     cartIcon.textContent = sum;
   }
 
-  getTotal();
+}
