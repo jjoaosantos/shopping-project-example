2d669578e019998e0e517477d9ae4a7c5e86687a
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-12-16 08:58:55 -0300

    cart.js first steps

--- scripts/cart.js
+++ scripts/cart.js
@@ -11,7 +11,46 @@ fetch("../scripts/products.json")
           if (!localStorage.getItem("cart")) {
             localStorage.setItem("cart", "[]");
           }
-          initialize(data);
          } )
   
   .catch( err => console.error(`Fetch problem: ${err.message}`) );
+
+  const cart = JSON.parse(localStorage.getItem("cart"));
+  const cartIcon = document.querySelector(".cart-icon");
+  const main = document.querySelector("main");
+
+  let finalGroup = [];
+  finalGroup = cart.filter( product => product.image.includes(product.image));
+  console.log(finalGroup);
+
+  // function addItemToCart() {
+  //   // const item = cart.filter( item => {
+  //   //   return item.image;
+  //   // });
+
+  //   const product = cart.filter( item => item.image.includes(item.image));
+
+  //   const section = document.createElement("section");
+  //   const image = document.createElement("img");
+  //   // section.setAttribute("class", `${cart.branding}`)
+  //   // image.src = `../images/${product}`;
+  //   main.appendChild(section);
+  //   section.appendChild(image);
+  //   console.log(product);
+  // }
+
+  // addItemToCart();
+
+  function getTotal() {
+    const temp = cart.map( item => {
+      return parseInt(item.quantity);
+      // return parseInt(item.price);
+    });
+
+    const sum = temp.reduce( function(prev, next) {
+      return prev + next;
+    }, 0);
+    cartIcon.textContent = sum;
+  }
+
+  getTotal();
