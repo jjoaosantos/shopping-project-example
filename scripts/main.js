f0acd2af8ec2f2d5f778bafd0a70f2b3ea078ac7
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-12-25 10:32:54 -0300

    adding heartBtn

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,6 @@ const menuBtn = document.getElementById("menuBtn");
 const navBar = document.getElementById("menuNavigator");
 const list = document.querySelector("#list");
 const ul = document.getElementById("list");
-const container = document.querySelector(".container");
-const img = document.querySelector(".image");
 
 navBar.style.left = "-100vw";
 list.style.display = "none";
@@ -18,7 +16,7 @@ menuBtn.addEventListener("click", () => {
         html.style.overflow = "hidden";
         navBar.style.overflow = "auto";
         list.style.display = "block";
-    } else if (menuBtn.getAttribute("aria-expanded") === "true") {
+    } else {
         menuBtn.setAttribute("aria-expanded", "false");
         menuBtn.style.backgroundImage = "url('../images/menu-bar.svg')";
         menuBtn.style.backgroundSize = "contain";
@@ -27,3 +25,19 @@ menuBtn.addEventListener("click", () => {
         list.style.display = "none";
     }
 });
+
+const section = document.querySelector("footer section");
+const heartBtn = document.querySelector("#heartBtn");
+heartBtn.addEventListener("click", () => {
+    if (heartBtn.getAttribute("aria-expanded") === "false") {
+        heartBtn.setAttribute("aria-expanded", "true");
+        const para = document.createElement("p");
+        para.textContent = "1";
+        section.appendChild(para);
+        
+    } else {
+        heartBtn.setAttribute("aria-expanded", "false");
+        const para = document.querySelector("footer section p");
+        section.removeChild(para);
+    }
+})
