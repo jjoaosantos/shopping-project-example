fbb72ad96744619d5d2e56c1c93e8d5399df4b55
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2022-12-25 22:48:57 -0300

    blur menu example

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const html = document.querySelector("html");
+const body = document.querySelector("body");
 const menuBtn = document.getElementById("menuBtn");
 const navBar = document.getElementById("menuNavigator");
 const list = document.querySelector("#list");
@@ -9,6 +10,9 @@ list.style.display = "none";
 
 menuBtn.addEventListener("click", () => {
     if (menuBtn.getAttribute("aria-expanded") === "false") {
+        const blur = document.createElement("div");
+        blur.setAttribute("class", "menu-expanded");
+        blur.style.backgroundColor = "rgba(0, 0, 0, 0.808)";
         menuBtn.setAttribute("aria-expanded", "true");
         menuBtn.style.backgroundImage = "url('../images/x-icon.svg')";
         menuBtn.style.backgroundSize = "80%";
@@ -16,13 +20,16 @@ menuBtn.addEventListener("click", () => {
         html.style.overflow = "hidden";
         navBar.style.overflow = "auto";
         list.style.display = "block";
+        body.appendChild(blur)
     } else {
+        const blur = document.querySelector(".menu-expanded");
         menuBtn.setAttribute("aria-expanded", "false");
         menuBtn.style.backgroundImage = "url('../images/menu-bar.svg')";
         menuBtn.style.backgroundSize = "contain";
         navBar.style.left = "-100vw";
         html.style.overflow = "auto";
         list.style.display = "none";
+        body.removeChild(blur)
     }
 });
 
