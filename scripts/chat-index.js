4b8b4246173f49110615d0c19fcd347be3b4cad6
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2023-01-03 21:05:45 -0300

    setting evening period on whatsapp chat

--- scripts/chat-index.js
+++ scripts/chat-index.js
@@ -12,18 +12,22 @@ function nameDisplayCheck() {
         const name = localStorage.getItem("name");
         if (hour > 0 && hour <= 12) {
             personalGreeting.textContent = `Good morning, ${name}. How can I help you?`;
-        } else if (hour > 12 && hour <= 22) {
+        } else if (hour > 12 && hour <= 4) {
             personalGreeting.textContent = `Good afternoon, ${name}. How can I help you?`;
-        } else if (hour > 22 && hour <= 0) {
+        } else if (hour > 4 && hour <= 21) {
+            personalGreeting.textContent = `Good evening, ${name}. How can I help you?`;
+        } else if (hour > 21 && hour <= 0) {
             personalGreeting.textContent = `Good night, ${name}. How can I help you?`;
         }
         
     } else {
         if (hour > 0 && hour <= 12) {
             personalGreeting.textContent = "Good morning. How can I help you?";
-        } else if (hour > 12 && hour <= 22) {
+        } else if (hour > 12 && hour <= 4) {
             personalGreeting.textContent = "Good afternoon. How can I help you?";
-        } else if (hour > 22 && hour <= 0) {
+        } else if (hour > 4 && hour <= 21) {
+            personalGreeting.textContent = `Good evening. How can I help you?`;
+        } else if (hour > 21 && hour <= 0) {
             personalGreeting.textContent = "Good night. How can I help you?";
         }
     }
