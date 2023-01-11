b50e60b45d5ddc69c53d253918060f2352779784
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2023-01-11 08:59:21 -0300

    translating social medias

---
+++ html/br/scripts/chat-index-br.js
@@ -0,0 +1,32 @@
+const chat = document.querySelector(".chatBtn");
+const main = document.querySelector("main");
+
+chat.addEventListener("click", nameDisplayCheck);
+
+function nameDisplayCheck() {
+    const personalGreeting = document.createElement("p");
+    const d = new Date();
+    let hour = d.getHours();
+    
+    if (localStorage.getItem("name")) {
+        const name = localStorage.getItem("name");
+        if (hour > 0 && hour <= 12) {
+            personalGreeting.textContent = `Bom dia, ${name}. Como posso ajudá-lo?`;
+        } else if (hour > 12 && hour <= 5) {
+            personalGreeting.textContent = `Boa tarde, ${name}. Como posso ajudá-lo?`;
+        } else if (hour > 5 && hour <= 0) {
+            personalGreeting.textContent = `Boa noite, ${name}. Como posso ajudá-lo?`;
+        }
+    } else {
+        if (hour > 0 && hour <= 12) {
+            personalGreeting.textContent = `Bom dia. Como posso ajudá-lo?`;
+        } else if (hour > 12 && hour <= 5) {
+            personalGreeting.textContent = `Boa tarde. Como posso ajudá-lo?`;
+        } else if (hour > 5 && hour <= 0) {
+            personalGreeting.textContent = `Boa noite. Como posso ajudá-lo?`;
+        }
+    }
+
+    chat.style.display = "none";
+    main.appendChild(personalGreeting);
+}
