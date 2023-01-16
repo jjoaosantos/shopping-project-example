ba07b58d42fcca6ccf17f4c57187ae394a963cab
jjoaosantos <92317031+jjoaosantos@users.noreply.github.com>
2023-01-16 19:47:24 -0300

    Finished

--- html/br/scripts/chat-index-br.js
+++ html/br/scripts/chat-index-br.js
@@ -9,20 +9,20 @@ function nameDisplayCheck() {
     
     if (localStorage.getItem("name")) {
         const name = localStorage.getItem("name");
-        if (hour > 0 && hour <= 12) {
+        if (hour > 23 && hour <= 12) {
             personalGreeting.textContent = `Bom dia, ${name}. Como posso ajudá-lo?`;
-        } else if (hour > 12 && hour <= 5) {
+        } else if (hour > 12 && hour <= 17) {
             personalGreeting.textContent = `Boa tarde, ${name}. Como posso ajudá-lo?`;
-        } else if (hour > 5 && hour <= 0) {
+        } else if (hour > 17 && hour <= 23) {
             personalGreeting.textContent = `Boa noite, ${name}. Como posso ajudá-lo?`;
         }
     } else {
-        if (hour > 0 && hour <= 12) {
-            personalGreeting.textContent = `Bom dia. Como posso ajudá-lo?`;
-        } else if (hour > 12 && hour <= 5) {
-            personalGreeting.textContent = `Boa tarde. Como posso ajudá-lo?`;
-        } else if (hour > 5 && hour <= 0) {
-            personalGreeting.textContent = `Boa noite. Como posso ajudá-lo?`;
+        if (hour > 23 && hour <= 12) {
+            personalGreeting.textContent = "Bom dia. Como posso ajudá-lo?";
+        } else if (hour > 12 && hour <= 17) {
+            personalGreeting.textContent = "Boa tarde. Como posso ajudá-lo?";
+        } else if (hour > 17 && hour <= 23) {
+            personalGreeting.textContent = "Boa noite. Como posso ajudá-lo?";
         }
     }
 
